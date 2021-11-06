33aeca8220d189a3d07113e00fc358a5c0e59b45
linux-admin0001 <igorkorobigor@gmail.com>
2021-11-06 18:22:53 +0300

    da

--- src/server.py
+++ src/server.py
@@ -1,14 +1,16 @@
+# !!! WARNING - TEST SERVER  (не для продакшна) !!! 
+
 import socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind(('', 9265))
-client = []  # Массив где храним адреса клиентов
+client = []
 print('Start Server')
 while 1:
     data, addres = sock.recvfrom(1024)
     print(f"{addres[0]}:{addres[1]} cords = {data}")
     if addres not in client:
-        client.append(addres)  # Если такого клиента нету , то добавить
+        client.append(addres)
     for clients in client:
         if clients == addres:
-            continue  # Не отправлять данные клиенту, который их прислал
+            continue
         sock.sendto(data, clients)
