816e70f731966b43fd0f949e0a36768a6b1b4af6
linux-admin0001 <igorkorobigor@gmail.com>
2022-06-05 14:14:47 +0300

    added event handler

--- src/game.zig
+++ src/game.zig
@@ -1,15 +1,18 @@
 const utils = @import("utils.zig");
 const Tank = @import("objects/tank.zig");
+const Handler = @import("handlers/winevents.zig").Handler;
 const sf = @import("sf");
 
 pub fn mainloop() !void {
     var window = try utils.create_window(200, 200, "TwisteRTanks");
+    var handler = Handler.create(&window);
     var tank = try Tank.create();
 
     errdefer window.destroy();
     defer tank.destroy();
 
     while (window.isOpen()) {
+        handler.update();
         window.clear(sf.Color.Black);
         window.draw(tank.sprite, null);
         window.display();
