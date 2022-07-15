4bfdc5fe527e92b6fc70adabc1d6e288664c6948
linux-admin0001 <igorkorobigor@gmail.com>
2022-07-15 19:44:50 +0300

    fixed seg fault

--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,12 @@
 const std = @import("std");
 const sf = @import("sf");
-//const game = @import("game[deprecated].zig");
-const game = @import("game.zig");
+const Game = @import("game.zig");
+
 pub fn main() !void {
-    _ = (try (try game.createGame()).runMainLoop());
-    //_ = try game.mainloop();
+    var game = try Game.createGame();
+    try game.setup();
+    try game.runMainLoop();
+
 }
 
 
