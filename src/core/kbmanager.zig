b9751c3e700dc46d07512178bdc9d244c6171b77
linux-admin0001 <igorkorobigor@gmail.com>
2022-07-19 13:47:15 +0300

    fixed errors in the code of `kbmanager.zig`

--- src/core/kbmanager.zig
+++ src/core/kbmanager.zig
@@ -8,11 +8,11 @@ const EventWrapper = @import("eventWrapper.zig").EventWrapper;
 const GameEvent = @import("gameEvent.zig").gameEvent;
 const HashMap = std.HashMap;
 
-pub fn addReactOnKey(self: *Self, binding: fn(*Game) anyerror!void, key: sf.Keyboard.KeyCode) !void {
+pub fn addReactOnKey(self: *Self, binding: fn(*Game) anyerror!void, key: sf.keyboard.KeyCode) !void {
     try self.keyMap.put(key, @ptrToInt(binding));
 }
 
-pub fn create(source: *Game) !Self {
+pub fn create(source: *Game) Self {
     return Self {
         .keyMap = std.AutoHashMap(sf.window.keyboard.KeyCode, usize).init(std.heap.page_allocator),
         .source = source
@@ -20,13 +20,19 @@ pub fn create(source: *Game) !Self {
 }
 
 pub fn update(self: *Self) !void {
-    for (self.keyMap.keys) |kcode| {
-        if sf.window.keyboard.isKeyPressed(kcode) {
-           var bindFuncPtrAddr: usize = self.keyMap.get(kcode).?;
-           var bindFunc = @intToPtr(fn(*Game) anyerror!void, bindFuncPtrAddr);
-           try bindFunc(self.source);
-        };
-    };
+
+    var it = self.keyMap.iterator();
+
+    while (it.next()) |kv| {
+        var kcode: sf.window.keyboard.KeyCode = kv.key_ptr.*;
+        var bindingAddr: usize = kv.value_ptr.*;
+        var binding: fn(*Game) anyerror!void = @intToPtr(fn(*Game) anyerror!void, bindingAddr);
+        
+        if (sf.window.keyboard.isKeyPressed(kcode)) {
+            try binding(self.source);
+        }
+    
+    }
 }
 
 keyMap: std.AutoHashMap(sf.window.keyboard.KeyCode, usize),
