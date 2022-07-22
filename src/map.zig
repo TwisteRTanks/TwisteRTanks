474a6eaff768c41c4aab060a90b86cf86fc9fb8d
linux-admin0001 <igorkorobigor@gmail.com>
2022-07-22 22:09:28 +0300

    fixed `expected error union type, found 'void'

--- src/map.zig
+++ src/map.zig
@@ -29,7 +29,7 @@ pub fn genMap(self: *Self) !void {
 
 pub fn drawOnWindow(self: Self, window: *sf.RenderWindow) void {
     for (self.tiles) |tile| {
-        try tile.drawOnWindow(window);
+        tile.drawOnWindow(window);
     }
 }
 tiles: [176]Tile,
