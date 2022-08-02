cd428b43420a0a0cf06bcba246e9bcc99212d52e
linux-admin0001 <igorkorobigor@gmail.com>
2022-08-02 18:11:58 +0300

    updated `addButton`, oth fixes

--- src/ui/menu.zig
+++ src/ui/menu.zig
@@ -6,13 +6,14 @@ const Button = @import("button.zig").Button;
 const InputField = @import("inputField.zig");
 const Label = @import("label.zig");
 const Menu = @import("menu.zig");
+const Game = @import("../game.zig");
 
 const MenuStyle = enum(c_int) {
     Horizontal,
     Vertical
 };
 
-pub fn create(title: [:0]const u8, pos: sf.Vector2f) !Self {
+pub fn create(game: *Game, title: [:0]const u8, pos: sf.Vector2f) !Self {
     var text = try sf.Text.createWithText(
             title, 
             try sf.Font.createFromFile(
@@ -20,10 +21,15 @@ pub fn create(title: [:0]const u8, pos: sf.Vector2f) !Self {
             ),
             64
     );
+    text.setPosition(pos);
+    text.setFillColor(sf.Color.fromRGB(255, 215, 0));
+
     return Self{
+        .game = game,
         .title = text,
         .buttons = std.ArrayList(*Button).init(std.heap.page_allocator),
         .position = pos,
+        .width = text.getLocalBounds().width,
     };
 }
 
@@ -35,10 +41,31 @@ pub fn destroy(self: *Self) void {
     }
 }
 
-/// The `button` parameter accepts a button with relative coordinates
-pub fn addButton(self: *Self, button: Button) !void {
+/// The `button` parameter accepts a button with **relative** coordinates
+pub fn addButton(self: *Self, label: [:0]const u8) !void {
 
-    button.setPosition(.{0,0});
+    const topButton: ?*Button = self.buttons.popOrNull();
+    
+    // TODO: refactor this code
+    if (topButton != null) {
+        try self.buttons.append(topButton.?);
+    }
+
+    var topButtonCords: sf.Vector2f = undefined;
+    var height: f32 = 0;
+    if (topButton == null){
+        topButtonCords = sf.Vector2f.new(0, 70);
+    } else {
+        topButtonCords = topButton.?.position;
+        height = topButton.?.body.getLocalBounds().height;
+    }
+
+    const tbcY: f32 = topButtonCords.y + height + 5;
+
+    const tbPos: [2]f32 = .{self.position.x, tbcY + self.position.y};
+    
+    // Now button have absolute position
+    var button: Button = try Button.create(self.game, tbPos, label, sf.Vector2f.new(self.width, 70));
 
     const buttonPtr = try std.heap.page_allocator.create(Button);
     buttonPtr.* = button;
@@ -46,6 +73,7 @@ pub fn addButton(self: *Self, button: Button) !void {
 }
 
 pub fn drawOnWindow(self: *Self, window: *sf.RenderWindow) void {
+    window.draw(self.title, null);
     for (self.buttons.items) |buttonPtr| {
         buttonPtr.*.drawOnWindow(window);
     }
@@ -57,7 +85,9 @@ pub fn update(self: *Self) void {
     }
 }
 
+game: *Game,
 title: sf.Text,
 buttons: std.ArrayList(*Button),
 position: sf.Vector2f,
+width: f32 = 200,
 //supplier: sf.RenderWindow,
